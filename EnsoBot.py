cf1225f420ce4dc0bbe010f403a12643002067d7
sgoudham <sgoudham@gmail.com>
2020-06-03 18:23:55 +0100

    I think it's working?

--- EnsoBot.py
+++ EnsoBot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 # Bot Prefix
 client = commands.Bot(command_prefix='~')
-token = open("token.txt", "r").readline()
+token = open("token/token.txt", "r").readline()
 
 # Instantiates a list for all the cogs
 extensions = ['cogs.WaifuImages', 'cogs.FunCommands']

080acc94785c17345e63db2e110599f13ebd5325
sgoudham <sgoudham@gmail.com>
2020-06-17 18:11:22 +0100

    Changed status of Enso-chan

--- EnsoBot.py
+++ EnsoBot.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 @client.event
 async def on_ready():
     print('Bot is ready.')
-    await client.change_presence(activity=discord.Activity(name="Falling in Love 😍 😍"))
+    await client.change_presence(activity=discord.Game(name="Getting Married!! 😍 💍"))
 
 
 # Bot ~Ping command in milliseconds

b905a84eccb0eb22f95c1956cf01daea86f10523
sgoudham <sgoudham@gmail.com>
2020-06-30 19:32:16 +0100

    Removing the command ~rolemenu

--- EnsoBot.py
+++ EnsoBot.py
@@ -91,53 +91,6 @@ async def on_member_join(member):
     await general.send(general_welcome)
 
 
-# Allowing people to get ping-able self roles
-@client.command(name="rolemenu")
-@is_owner()
-async def role_menu(ctx):
-    # Surround with try/except to catch any exceptions that may occur
-    try:
-
-        # Get the channel id of #self-roles
-        channel = client.get_channel(722347423913213992)
-
-        # Set up embed to let people know what ping-able roles can be chosen
-        embed = Embed(title="**Role Menu: Ping-Able Roles**",
-                      colour=Colour.orange(),
-                      timestamp=datetime.datetime.utcnow())
-
-        embed.set_thumbnail(url="https://media.discordapp.net/attachments/683490529862090814/715010931620446269"
-                                "/image1.jpg?width=658&height=658")
-        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
-        embed.set_footer(text=f"{ctx.message.author}", icon_url=ctx.author.avatar_url)
-        embed.add_field(
-            name="\u200b",
-            value="React to give yourself roles to be pinged for these events!",
-            inline=False)
-        embed.add_field(
-            name="\u200b",
-            value="<:MovieNight:722293598938333190> : `Movie Nights`",
-            inline=False)
-        embed.add_field(
-            name="\u200b",
-            value="<:Karaoke:722358251932483605> : `Karaoke Nights`",
-            inline=False)
-        embed.add_field(
-            name="\u200b",
-            value="<:EnsoBros:722360289345011743> : `Enso Bros Podcasts`",
-            inline=False)
-        embed.add_field(
-            name="\u200b",
-            value="<:GameNights:722502073769525268> : `Game Nights`",
-            inline=False)
-
-        # Send embed to #self-roles
-        await channel.send(embed=embed)
-
-    except Exception as ex:
-        print(ex)
-
-
 # ~marry command allows the bot to wed two young lovers together
 @client.command(name="marry", aliases=["Marry"])
 async def marry(ctx, member: Member):

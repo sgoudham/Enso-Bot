ae00c12f00f85fdc17e1c79a182ebc318908863e
sgoudham <sgoudham@gmail.com>
2020-07-01 15:55:29 +0100

    Removing channel "general" Replacing it with an ID

--- settings.py
+++ settings.py
@@ -40,7 +40,7 @@ colors = {
 }
 
 # Allowed channel for Enso~Chan commands
-channels = ["enso-chan-commands", "picto-chat", "general"]
+channels = ["enso-chan-commands", "picto-chat", 663651584399507481]
 
 # Grabbing the list of colours
 colour_list = [c for c in colors.values()]

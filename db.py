75a6b3b4a85c01fe0250baaf57188035b885fcfa
sgoudham <sgoudham@gmail.com>
2020-07-09 23:02:15 +0100

    Updating Database

--- db.py
+++ db.py
@@ -10,9 +10,9 @@ def connection():
     # Connect to MariaDB Platform
     try:
         conn = mariadb.connect(
-            user="root",
+            user="root@%",
             password=password,
-            host="localhost",
+            host="173.208.202.20",
             port=3306,
             database="enso"
         )

f268d68ca04a5f2f01e745479d69e65bf6002619
sgoudham <sgoudham@gmail.com>
2020-07-11 02:55:21 +0100

    Removing streaming from play

--- cogs/music.py
+++ cogs/music.py
@@ -87,7 +87,7 @@ class Music(commands.Cog):
         """Streams from a url (same as yt, but doesn't predownload)"""
 
         async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
             ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
         await ctx.send('Now playing: {}'.format(player.title))

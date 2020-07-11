8d3c8e1b1608eca104e39a8ee7165830a76b7a70
sgoudham <sgoudham@gmail.com>
2020-07-11 18:30:02 +0100

    Adding Cog

--- cogs/music.py
+++ cogs/music.py
@@ -175,7 +175,7 @@ class MusicPlayer:
         return self.bot.loop.create_task(self._cog.cleanup(guild))
 
 
-class Music:
+class Music(commands.Cog):
     """Music related commands."""
 
     __slots__ = ('bot', 'players')

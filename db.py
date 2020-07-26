32f5355a0a016bacd6603d7558614bff3881846c
sgoudham <sgoudham@gmail.com>
2020-07-26 17:48:27 +0100

    Using the new asynchronous db connections

--- db.py
+++ db.py
@@ -1,11 +1,30 @@
+import asyncio
 import sys
 
+import aiomysql
 import mariadb
 from decouple import config
 
 # Get password/host from .env
 password = config('DB_PASS')
 host = config('DB_HOST')
+
+
+# Setting up connection using pool/aiomysql
+async def connection2(loop):
+    pool = await aiomysql.create_pool(
+        host=host,
+        port=3306,
+        user="hamothy",
+        password=password,
+        db='enso',
+        loop=loop)
+
+    return pool
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(connection2(loop))
 
 
 # Connect to MariaDB Platform and database Enso

b3cb2e30d2aae4c398a12fbf8d70513d711ebe58
sgoudham <sgoudham@gmail.com>
2020-08-22 04:59:35 +0100

    Got rid of the testing

--- main.py
+++ main.py
@@ -14,29 +14,11 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-from discord.ext.commands import is_owner
 
 from bot import Bot
 
 # Initiating Bot Object As Client
 client = Bot()
-
-
-@client.command()
-@is_owner()
-async def test(ctx):
-    # Alt Account
-    testing1 = await client.check_cache(556665878662348811, 621621615930638336)
-    print(client.member_cache.cache)
-
-    # My Account on the same guild as Alt
-    testing3 = await client.check_cache(154840866496839680, 621621615930638336)
-    print(client.member_cache.cache)
-
-    # Me in another guild
-    testing4 = await client.check_cache(154840866496839680, 663651584399507476)
-    print(client.member_cache.cache)
-
 
 # Run the bot
 client.execute()

68b65fce4f2dfde805509a1b8fb13ac3424ad5fa
Hammy <sgoudham@gmail.com>
2021-09-18 13:35:31 +0100

    Update and add more annotations

--- src/main/java/me/goudham/command/annotation/SubCommand.java
+++ src/main/java/me/goudham/command/annotation/SubCommand.java
@@ -7,13 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Qualifier
-@Target(ElementType.TYPE)
+@Target(ElementType.ANNOTATION_TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Command {
+public @interface SubCommand {
     String name();
     String description();
-    boolean isVisible();
-    SubCommandGroup[] subCommandGroups() default {};
-    SubCommand[] subCommands() default {};
     Option[] options() default {};
 }

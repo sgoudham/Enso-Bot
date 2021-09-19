0ddb21eec038811e10fd699468444a94c7ffd182
Hammy <sgoudham@gmail.com>
2021-09-19 01:19:11 +0100

    Rename @Command to @SlashCommand & Add Micronaut Annotations

--- src/main/java/me/goudham/command/annotation/SlashCommand.java
+++ src/main/java/me/goudham/command/annotation/SlashCommand.java
@@ -1,15 +1,19 @@
 package me.goudham.command.annotation;
 
+import io.micronaut.core.annotation.Introspected;
 import jakarta.inject.Qualifier;
+import jakarta.inject.Singleton;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Singleton
+@Introspected
 @Qualifier
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Command {
+public @interface SlashCommand {
     String name();
     String description();
     boolean isVisible();

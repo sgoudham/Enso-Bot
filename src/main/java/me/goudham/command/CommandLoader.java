cfef147fe721575c8c869a198acbcec5dc7ee1b0
Hammy <sgoudham@gmail.com>
2021-09-21 01:46:55 +0100

    Return Pair<Bean, Method>

--- src/main/java/me/goudham/command/CommandLoader.java
+++ src/main/java/me/goudham/command/CommandLoader.java
@@ -1,10 +1,11 @@
 package me.goudham.command;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
-import me.goudham.bot.command.ISlashCommand;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
 
 public interface CommandLoader {
-    List<CommandData> loadIntoMapAndReturnCommands(Map<String, ISlashCommand> commandMap);
+    List<CommandData> loadIntoMapAndReturnCommands(Map<String, Pair<Object, Method>> commandMap);
 }

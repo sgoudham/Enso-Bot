8c7ab5c4aca10c976c440ee1517713b2c28c78de
Hammy <sgoudham@gmail.com>
2021-09-22 01:36:44 +0100

    Use ExecutableMethod instead of Method.java

--- src/main/java/me/goudham/command/CommandLoader.java
+++ src/main/java/me/goudham/command/CommandLoader.java
@@ -1,11 +1,11 @@
 package me.goudham.command;
 
-import java.lang.reflect.Method;
+import io.micronaut.inject.ExecutableMethod;
 import java.util.List;
 import java.util.Map;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.internal.utils.tuple.Pair;
 
 public interface CommandLoader {
-    List<CommandData> loadIntoMapAndReturnCommands(Map<String, Pair<Object, Method>> commandMap);
+    List<CommandData> loadIntoMapAndReturnCommands(Map<String, Pair<Object, ExecutableMethod<Object, Object>>> commandMap);
 }

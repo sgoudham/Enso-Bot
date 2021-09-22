09c54c80d92806008f1d68c3734aa187cc2fe677
Hammy <sgoudham@gmail.com>
2021-09-22 01:39:27 +0100

    Update commands to use new subcommand methods

--- src/main/java/me/goudham/bot/command/info/Ping.java
+++ src/main/java/me/goudham/bot/command/info/Ping.java
@@ -1,28 +1,28 @@
 package me.goudham.bot.command.info;
 
+import io.micronaut.context.annotation.Executable;
 import jakarta.inject.Inject;
-import me.goudham.bot.command.ISlashCommand;
 import me.goudham.command.annotation.SlashCommand;
-import me.goudham.service.EmbedGenerator;
+import me.goudham.service.EmbedService;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 
 @SlashCommand(name = "ping", description = "Display the latency of the bot", isVisible = true)
-public class Ping implements ISlashCommand {
-    private final EmbedGenerator embedGenerator;
+public class Ping {
+    private final EmbedService embedService;
 
     @Inject
-    public Ping(EmbedGenerator embedGenerator) {
-        this.embedGenerator = embedGenerator;
+    public Ping(EmbedService embedService) {
+        this.embedService = embedService;
     }
 
-    @Override
+    @Executable
     public void handle(SlashCommandEvent event) {
         event.deferReply(false).queue();
         InteractionHook hook = event.getHook();
         JDA jda = event.getJDA();
 
-        jda.getRestPing().queue(ping -> hook.sendMessageEmbeds(embedGenerator.getBaseEmbed().setDescription("**Rest ping: " + ping + "ms**\n**WS ping: " + jda.getGatewayPing() + "ms**").build()).queue());
+        jda.getRestPing().queue(ping -> hook.sendMessageEmbeds(embedService.getBaseEmbed().setDescription("**Rest ping: " + ping + "ms**\n**WS ping: " + jda.getGatewayPing() + "ms**").build()).queue());
     }
 }

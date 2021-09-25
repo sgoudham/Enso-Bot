7becdc4528c2ea6f6d68c3169529343f8fa2ff4f
Hammy <sgoudham@gmail.com>
2021-09-25 08:28:51 +0100

    Add Constants.java for emotes

---
+++ src/main/java/me/goudham/domain/Constants.java
@@ -0,0 +1,28 @@
+package me.goudham.domain;
+
+public class Constants {
+    public static final String CHECK = "<:greenTick:746834932936212570>";
+    public static final String CROSS = "<:xMark:746834944629932032>";
+
+    public static final String STATUS_ONLINE = "<:statusOnline:891150224905633882>";
+    public static final String STATUS_OFFLINE = "<:statusOffline:891150224872063026>";
+    public static final String STATUS_IDLE = "<:statusIdle:891150224859484170>";
+    public static final String STATUS_DND = "<:statusDND:891150224842694716>";
+    public static final String STATUS_STREAMING = "<:statusStreaming:891159310430785567>";
+
+    public static final String ICON_GITHUB = "<:github:741000905364603010>";
+    public static final String ICON_TRASH = "<:trash:741055830274605146>";
+    public static final String ICON_INVITE = "<:invite:740998357643952139>";
+
+    public static final String BADGE_BALANCE = "<:badgeBalance:891150224637165688>";
+    public static final String BADGE_BRAVERY = "<:badgeBravery:891150224549097502>";
+    public static final String BADGE_BRILLIANCE = "<:badgeBrilliance:891150224620392478>";
+    public static final String BADGE_STAFF = "<:badgeStaff:891150225077583872>";
+    public static final String BADGE_PARTNER = "<:badgePartner:891150224691703818>";
+    public static final String BADGE_BOT = "<:badgeBot:891150224578457611>";
+    public static final String BADGE_VERIFIED_BOT = "<:badgeVerifiedBot:891150224687521813>";
+    public static final String BADGE_BUG_HUNTER = "<:badgeBugHunter:891153269701890089>";
+    public static final String BADGE_EARLY_SUPPORTER = "<:badgeESupporter:891150224502951967>";
+    public static final String BADGE_EARLY_VERIFIED_BOT_DEVELOPER = "<:badgeBotDev:891150224595238942>";
+    public static final String BADGE_SERVER_BOOST = "<:badgeServerBoost:891150224578453545>";
+}
